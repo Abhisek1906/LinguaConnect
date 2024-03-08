d7edf6d2b38c0f4e12c73982ab4a02f36b9a698d
Abhisek1906 <abhisekmishra150@gmail.com>
2024-03-08 22:26:50 +0530

    First Commit.

---
+++ src/main/java/com/example/linguaconnect/LinguaConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.linguaconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinguaConnectApplication.class, args);
+    }
+
+}
